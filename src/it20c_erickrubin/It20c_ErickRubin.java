087862a7a13f473820a422c3b5dc0391983e0053
ErickJamesRubin <rubin.erick123@gmail.com>
2024-09-22 23:36:42 +0800

    MY JAVA PROJECT

---
+++ src/it20c_erickrubin/It20c_ErickRubin.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it20c_erickrubin;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class It20c_ErickRubin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String [] Motors = {"AEROX 155","Sniper 150","Ducatti 1000cc","Honda Click","XRM 125"};  
+        // Array 'Cars' is initialized with 5 car names
+        
+        System.out.println("Some list of fascinating motors: ");  
+        // Prints an introductory message
+        
+        // Loop through each element in the 'Cars' array
+        for (int i = 0; i < Motors.length; i++){  
+            // Prints each car name from the array
+            System.out.println(Motors[i]);
+        }
+    }
+    
+}
