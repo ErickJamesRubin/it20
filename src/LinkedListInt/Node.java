c9ef4c9578225df769f3046996c6660ffaaac54b
ErickJamesRubin <rubin.erick123@gmail.com>
2024-09-27 20:59:21 +0800

    LinkedList_Int Program

---
+++ src/LinkedListInt/Node.java
@@ -0,0 +1,12 @@
+package LinkedListInt;
+
+public class Node {
+    int data;  
+    Node next;  
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
