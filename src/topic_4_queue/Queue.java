7058289fd8a5e5d7a0a6cda132a9089387fc171a
ErickJamesRubin <rubin.erick123@gmail.com>
2024-10-15 20:45:58 +0800

    Implemented a Queue class using LinkedList to manage Customer objects.

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package topic_4_queue;
 
-/**
- *
- * @author Erick James Rubin
- */
 public class Queue {
-    
+    private LinkedList<Customer> customers;
+
+    public Queue() {
+        customers = new LinkedList<>();
+    }
 }
