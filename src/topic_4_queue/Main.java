f00f3e9bb16d159c4473d7b44e885a4426e3c34e
ErickJamesRubin <rubin.erick123@gmail.com>
2024-10-15 21:18:43 +0800

    Added functionality to enqueue multiple customers, serve one, and display the queue.

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 	Queue customerQueue = new Queue();
-        Scanner scanner = new Scanner(System.in);	
-		 
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter first customer name to add to the queue: ");
+        customerQueue.enqueue(new Customer(scanner.nextLine()));
+        
+        System.out.println("Enter second customer name to add to the queue: ");
+        customerQueue.enqueue(new Customer(scanner.nextLine()));
+        
+        System.out.println("Enter third customer name to add to the queue: ");
+        customerQueue.enqueue(new Customer(scanner.nextLine()));
+
+        System.out.println("Serving a customer...");
+        customerQueue.dequeue();
+
+        System.out.println("Displaying the queue:");
+        customerQueue.displayQueue();		 
     }
 }
