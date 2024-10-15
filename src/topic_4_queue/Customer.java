ddb8825ce9d95537a5c6c9af7f84f1d36daacb0c
ErickJamesRubin <rubin.erick123@gmail.com>
2024-10-15 20:14:43 +0800

    Added Customer class with name field and getter method

--- src/topic_4_queue/Customer.java
+++ src/topic_4_queue/Customer.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package topic_4_queue;
 
-/**
- *
- * @author Erick James Rubin
- */
 public class Customer {
-    
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
