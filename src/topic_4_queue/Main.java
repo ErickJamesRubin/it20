4166ec5f631b1e24c10c6630a5f433b4ea4a74ad
ErickJamesRubin <rubin.erick123@gmail.com>
2024-10-15 20:09:26 +0800

    Added main.java class in topic_4_queue package

---
+++ src/topic_4_queue/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package topic_4_queue;
+
+/**
+ *
+ * @author Erick James Rubin
+ */
+public class Main {
+    
+}
