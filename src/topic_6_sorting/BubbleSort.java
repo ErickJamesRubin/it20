14acd228870a57cc08b9a7d1aee522a5f9177b14
ErickJamesRubin <rubin.erick123@gmail.com>
2024-11-28 10:38:59 +0800

    Added a method to print the elements of the array.

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -15,4 +15,11 @@ public class BubbleSort {
         }
     }
     
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    
 }
