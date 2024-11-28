bb92f9108698baee973dbb18b70e426e82849584
ErickJamesRubin <rubin.erick123@gmail.com>
2024-11-28 10:49:07 +0800

    Added a method to print the array elements.

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -17,6 +17,13 @@ public class InsertionSort {
         }
     }
     
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    
     public static void main (String args []){
         
     }
