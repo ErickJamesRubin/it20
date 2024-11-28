355afd3c796f37117a8b414c1f0f4410ad07dfe7
ErickJamesRubin <rubin.erick123@gmail.com>
2024-11-28 09:54:38 +0800

    Implemented a recursive method to calculate the factorial of a number

--- src/topic_5_recursion/Factorial.java
+++ src/topic_5_recursion/Factorial.java
@@ -2,4 +2,15 @@ package topic_5_recursion;
 
 public class Factorial {
     
+    public int factorial(int n) {
+	        if (n == 0 || n == 1) {
+	            return 1;
+	        } else {
+	            return n * factorial(n - 1);
+	        }
+	    }
+    
+    public static void main (String args []){
+        
+    }
 }
