4de2dd197fcb099734baa86efb8555cbb6db4dde
ErickJamesRubin <rubin.erick123@gmail.com>
2024-11-28 10:50:04 +0800

    Added user input to enter array size and elements, then sorted and displayed the array.

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -25,6 +25,26 @@ public class InsertionSort {
     }
     
     public static void main (String args []){
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        
+        int[] arr = new int[size];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+        
+        scanner.close();
     }
 }
