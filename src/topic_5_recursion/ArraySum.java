9c1d5fd4489080f0fa3b51d4a5dacd3da7d9d6a6
ErickJamesRubin <rubin.erick123@gmail.com>
2024-11-28 09:46:09 +0800

    Added a recursive function to calculate the sum of an array.

--- src/topic_5_recursion/ArraySum.java
+++ src/topic_5_recursion/ArraySum.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package topic_5_recursion;
 
-/**
- *
- * @author ITLAB2-PC08-STUDENT
- */
 public class ArraySum {
+    public int sum(int[] arr, int n) {
+
+        if (n <= 0) {
+            return 0;
+        }
+
+        // Recursive case: sum of the last element and the sum of the rest
+        return arr[n - 1] + sum(arr, n - 1);
+    }
+    
+    
     
 }
